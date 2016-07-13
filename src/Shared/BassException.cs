fcb0aba986fb30a4cf822d3bd12251cc988cb72a
Mathew Sachin <Mathew.Sachin.Git@Outlook.com>
2016-07-13 21:50:06 +0530

    Public constructors for BassException

--- src/Shared/BassException.cs
+++ src/Shared/BassException.cs
@@ -3,13 +3,19 @@
 namespace ManagedBass
 {
     /// <summary>
-    /// An Exception wrapping <see cref="Bass.LastError"/> or <see cref="BassAsio.LastError"/>.
+    /// An Exception wrapping <see cref="Bass.LastError"/> or BassAsio.LastError.
     /// </summary>
     public class BassException : Exception
     {
-        internal BassException() : this(Bass.LastError) { }
+        /// <summary>
+        /// Creates a new instance of <see cref="BassException"/> with <see cref="Bass.LastError"/>.
+        /// </summary>
+        public BassException() : this(Bass.LastError) { }
 
-        internal BassException(Errors ErrorCode)
+        /// <summary>
+        /// Creates a new instance of <see cref="BassException"/> with provided Error code.
+        /// </summary>
+        public BassException(Errors ErrorCode)
             : base($"Error: {ErrorCode}")
         {
             this.ErrorCode = ErrorCode;
